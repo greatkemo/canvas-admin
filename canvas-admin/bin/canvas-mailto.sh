a431b45fde5f589451f69be81d27a90715d54e07
greatkemo <ktaynaz@gmail.com>
2020-04-09 10:45:49 +0300

    fixed header

--- canvas-admin/bin/canvas-mailto.sh
+++ canvas-admin/bin/canvas-mailto.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# @Author: Kamal Taynaz <ktaynaz>
+# @Author: Kamal Taynaz <GreatKemo>
 # @Date:   08-Apr-2020
 # @Email:  ktaynaz@gmail.com
 # @Filename: canvas-mailto.sh
-# @Last modified by:   ktaynaz
+# @Last modified by:   GreatKemo
 # @Last modified time: 09-Apr-2020
 # @License: GNU General Public License v3.0
 
 #this script will email files to the admin.
 #source ldap variables from env directory
 source /usr/local/canvas-admin/env/canvas_admin.shenv
+
 # variables related to the logged in admin and mail message
 LOCAL_USER_ID="$(whoami)"
 LOCAL_USER_EMAIL="$(ldapsearch -x -LLL -h "${COMPANY_LDAP_HOST}" -b \
